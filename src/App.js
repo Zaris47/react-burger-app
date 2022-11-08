77413ade6d134e32f307be681fde399652a2123e
Zaris47 <akhildasmisterio@gmail.com>
2022-11-08 20:06:20 +0530

    Day 2 progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
-
+import Layout from "./components/Layout/Layout";
 function App() {
   return (
     <div>
-
+      <Layout>
+       <p>Test</p>
+      </Layout>
     </div>
   );
 }
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,8 @@
+const layout = (props) => (
+  <>
+    <div>Toolbar, Sidebar, Backdrop</div>
+    <main>{props.children}</main>
+  </>
+);
+
+export default layout;
